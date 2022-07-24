3a398842088827549cd2ab5fb1ff104a82d9d787
Eric Roos <ericroos@hey.com>
2022-07-24 18:55:59 -0500

    fix spec

--- spec/requests/inbox_spec.rb
+++ spec/requests/inbox_spec.rb
@@ -3,9 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Inboxes', type: :request do
+  before do
+    sign_in FactoryBot.create(:user)
+  end
   describe 'GET /index' do
     it 'returns http success' do
-      get '/inbox/index'
+      get '/inbox'
       expect(response).to have_http_status(:success)
     end
   end
