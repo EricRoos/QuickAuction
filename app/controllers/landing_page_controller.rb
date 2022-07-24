75ec4073b5bd25b18f819e13f0d4dd6a22eb8d09
Eric Roos <ericroos@hey.com>
2022-07-24 16:21:40 -0500

    open up landing page permissions

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class LandingPageController < ApplicationController
+  skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
   layout 'landing_page'
   def show; end
