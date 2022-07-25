f50c8ffd62426c6b14d1f357f5443419d84bd720
Eric Roos <ericroos@hey.com>
2022-07-25 09:58:47 -0500

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
     resources :auction_offers, except: [:edit], shallow: true
   end
 
-  if ((Rails.env.test? || Flipper.enabled?(:public_access)) rescue false)
-    devise_for :users
-  end
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
