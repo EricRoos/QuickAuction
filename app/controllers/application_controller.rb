22b1bd8a21a41a2981ae21b6a113ea211f73dd27
Eric Roos <ericroos@hey.com>
2022-07-26 16:42:55 -0500

    fix admin access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,15 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   before_action :check_public_access_enabled, if: -> { devise_controller? }
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { active_admin_request? }
   before_action :add_initial_breadcrumbs
 
   after_action :verify_authorized, unless: -> { devise_controller? || active_admin_request? }
 
   default_form_builder AppFormBuilder
 
-  def after_sign_in_path_for(_resource)
+  def after_sign_in_path_for(resource)
+    return admin_dashboard_path if resource.is_a?(AdminUser) 
     auction_items_path
   end
 
@@ -23,7 +24,6 @@ class ApplicationController < ActionController::Base
 
   def active_admin_request?
     request.path.starts_with?('/admin')
-    true
   end
 
   def add_initial_breadcrumbs
