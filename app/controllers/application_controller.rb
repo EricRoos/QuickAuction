513d75844d147dab77123e5d6cbb048ab727840e
Eric Roos <ericroos@hey.com>
2022-08-01 20:31:16 -0500

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,6 +61,7 @@ class ApplicationController < ActionController::Base
   end
 
   def verify_origin_server
+    Rails.logger.info("#{ENV['INTERNAL_CF_KEY_INSTAAUCTION']} == #{request.headers['INTERNAL_CF_KEY_INSTAAUCTION']}")
     return unless ENV['INTERNAL_CF_KEY_INSTAAUCTION'].present?
     return if ENV['INTERNAL_CF_KEY_INSTAAUCTION'] == request.headers['INTERNAL_CF_KEY_INSTAAUCTION']
 
