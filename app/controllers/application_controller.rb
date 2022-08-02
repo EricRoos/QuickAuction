9316fda48d74ff87e5c04be14061b44552233f12
Eric Roos <ericroos@hey.com>
2022-08-01 20:34:56 -0500

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,7 +61,8 @@ class ApplicationController < ActionController::Base
   end
 
   def verify_origin_server
-    Rails.logger.info("#{ENV['INTERNAL_CF_KEY_INSTAAUCTION']} == #{request.headers['INTERNAL_CF_KEY_INSTAAUCTION']}")
+    Rails.logger.info("#{ENV.fetch('INTERNAL_CF_KEY_INSTAAUCTION',
+                                   nil)} == #{request.headers['INTERNAL_CF_KEY_INSTAAUCTION']}")
     return unless ENV['INTERNAL_CF_KEY_INSTAAUCTION'].present?
     return if ENV['INTERNAL_CF_KEY_INSTAAUCTION'] == request.headers['INTERNAL_CF_KEY_INSTAAUCTION']
 
