9d85c0796d7fd4c33730507889ae3c3ff40bae0c
Eric Roos <ericroos@hey.com>
2022-08-05 21:04:10 -0600

    send messages

--- app/notifications/auction_closed_notification.rb
+++ app/notifications/auction_closed_notification.rb
@@ -1,32 +1,16 @@
 # frozen_string_literal: true
 
-# To deliver this notification:
-#
-# OfferUpdatedNotification.with(post: @post).deliver_later(current_user)
-# OfferUpdatedNotification.with(post: @post).deliver(current_user)
-
 class AuctionClosedNotification < Noticed::Base
-  # Add your delivery methods
-  #
   deliver_by :database
   # deliver_by :email, mailer: "UserMailer"
   # deliver_by :slack
   # deliver_by :custom, class: "MyDeliveryMethod"
 
-  # Add required params
-  #
   param :auction_item_id, :game_password, :game_name
 
-  # Define helper methods to make rendering easier.
-  #
-  # def message
-  #   t(".message")
-  # end
   def message
-    "Auction closed. Seller, please create a game with the name <b>#{params[:game_name]}</b> with password <b>#{params[:game_password]}</b>".html_safe
+    # rubocop:disable Layout/LineLength
+    "Auction closed. Seller, please create a game with the name <b>#{params[:game_name]}</b> with password <b>#{params[:game_password]}</b> in the NA region".html_safe
+    # rubocop:enable Layout/LineLength
   end
-  #
-  # def url
-  #   post_path(params[:post])
-  # end
 end
