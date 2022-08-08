3d901574c422f9ca53d54e2e5a8be9d338873671
Eric Roos <ericroos@hey.com>
2022-08-07 20:52:22 -0600

    clean up

--- spec/views/support_tickets/new.html.tailwindcss_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'support_tickets/new.html.tailwindcss', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
