a60deef190c29e2d3fffaeb497a6d3405667110f
Eric Roos <ericroos@hey.com>
2022-08-07 23:00:13 -0600

    enable flags in admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -14,6 +14,17 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  member_action :enable_feature, method: :post do
+    Flipper.enable_actor params[:feature], resource
+    redirect_to resource_path, notice: "enabled #{params[:feature]} for user##{resource.id}"
+  end
+
+  member_action :disable_feature, method: :post  do
+    Flipper.disable_actor params[:feature], resource
+    redirect_to resource_path, notice: "disabled #{params[:feature]} for user##{resource.id}"
+  end
+
   show do
     default_main_content
     panel "Tickets" do
@@ -26,6 +37,22 @@ ActiveAdmin.register User do
         column :updated_at
         column '' do |ticket|
           link_to 'View', admin_support_ticket_path(ticket)
+        end
+      end
+    end
+    panel "Features" do
+      table_for Flipper.features do
+        column :name
+        column :state do |feature|
+          Flipper.enabled? feature.name, user
+        end
+        column '' do |feature|
+          span do
+            button_to('Enable', enable_feature_admin_user_path(user, feature: feature))
+          end
+          span do
+            button_to('Disable', disable_feature_admin_user_path(user, feature: feature))
+          end
         end
       end
     end
